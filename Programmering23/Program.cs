5d9b8ea649b6f8716971b21c93305c42a3060ca1
Abd Alaal Hadi <haab05005@utb.tingsryd.se>
2023-10-09 10:59:18 +0200

    PR

--- Programmering23/Program.cs
+++ Programmering23/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 class Program
@@ -9,41 +8,40 @@ class Program
     {
        
         Random random = new Random();
-
-       
         List<int> abilityScores = new List<int>();
 
-        
+
+        List<int> rolls;
+        int rollone;
+        int rolltwo;
+        int rollthree;
+        int rollfour;
+
+
+        int totalrolls;
+
         for (int i = 0; i < 6; i++)
         {
-            int[] rolls = new int[4];
+            rolls = new List<int>();
 
-          
-            for (int j = 0; j < 4; j++)
-            {
-                rolls[j] = random.Next(1, 7); 
-            }
+            rollone = random.Next(1, 7);
+            rolltwo = random.Next(1, 7);
+            rollthree = random.Next(1, 7);
+            rollfour = random.Next(1, 7);
 
-          
-            Array.Sort(rolls);
-            Array.Reverse(rolls);
+            rolls.Add(rollone);
+            rolls.Add(rolltwo);
+            rolls.Add(rollthree);
+            rolls.Add(rollfour);
 
-        
-            int sum = rolls.Take(3).Sum();
+            rolls.Sort();
 
-         
-            abilityScores.Add(sum);
+            totalrolls = rolls[1] + rolls[2] + rolls[3];
 
-            
-            Console.WriteLine("You roll " + rolls[0] + ", " + rolls[1] + ", " + rolls[2] + ", " + rolls[3] + ". The ability score is " + sum + ".");
+            Console.WriteLine("Your available ability scores are " + String.Join (", ", rolls) +" total rolls " + totalrolls);
+            abilityScores.Add(totalrolls);
+
         }
-
- 
-        abilityScores.Sort();
-        abilityScores.Reverse();
-
-    
-        Console.WriteLine("\nYour available ability scores are " + string.Join(", ", abilityScores));
 
     }
 }
